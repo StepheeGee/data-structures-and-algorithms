e79f33e8363dfd8be4e445e0e32357cf8edbee14
STEPHANIE G JOHNSON <STEPHANIEJOHNSON.ESQ@GMAIL.COM>
2023-12-04 15:56:41 -0500

    out of order - completed challenge 6. still have 3-5

--- javascript/code-challenges/challenges-06.test.js
+++ javascript/code-challenges/challenges-06.test.js
@@ -123,7 +123,7 @@ Note: You must use reduce for this challenge. You may not use the built-in .reve
 ------------------------------------------------------------------------------------------------ */
 
 const reversedString = (str) => {
-  // Solution code here...
+  return str.split('').reduce((reversed, character) => character + reversed, '');
 };
 
 /* ------------------------------------------------------------------------------------------------
